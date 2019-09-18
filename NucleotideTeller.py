0fc3f0048b0fc9ec3a22ce2952486f2f6b4074e0
BramUt <55189626+BramUt@users.noreply.github.com>
2019-09-18 15:05:57 +0200

    Update NucleotideTeller.py

--- NucleotideTeller.py
+++ NucleotideTeller.py
@@ -1,14 +1,14 @@
-#Vragen naar de path van het fasta bestand en zet de inhoud ervan in een variabele
+# Vragen naar de path van het fasta bestand en zet de inhoud ervan in een variabele
 seq_path = input("FASTA path: ")
 seq = open(seq_path)
 sequentie = ""
 
-#Filteren lines die beginnen met > en der whitespaces halen uit de rest
+# Filteren lines die beginnen met > en der whitespaces halen uit de rest
 for regel in seq:
     if ">" not in regel:
         sequentie += regel.rstrip()
 
-#Definiëren van de variabele voor iedere nucleotide
+# Definiëren van de variabele voor iedere nucleotide
 nucleotide_A = "A"
 nucleotide_C = "C"
 nucleotide_G = "G"
@@ -17,17 +17,17 @@ nucleotide_N = "N"
 
 print(sequentie)
 
-#Het tellen van het aantal nucleotide per soort
+# Het tellen van het aantal nucleotide per soort
 aantal_A = sequentie.count(nucleotide_A)
 aantal_C = sequentie.count(nucleotide_C)
 aantal_G = sequentie.count(nucleotide_G)
 aantal_T = sequentie.count(nucleotide_T)
 aantal_N = sequentie.count(nucleotide_N)
 
-#Optellen totaal antal nucleotiden
+# Optellen totaal antal nucleotiden
 totaal = aantal_A + aantal_C + aantal_G + aantal_T + aantal_N
 
-#Percentage berekenen
+# Percentage berekenen
 procentCG = (aantal_C + aantal_G) / totaal *100
 
 print("Aantal nucleotiden: " , totaal)
